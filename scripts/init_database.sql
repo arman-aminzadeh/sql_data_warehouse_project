4f549a317258858b6e7fe67f334d11924ca7f50f
Arman Aminzadeh <60994642+arman-aminzadeh@users.noreply.github.com>
2026-01-22 20:48:24 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,9 @@
+/*Create Database*/
+
+DROP DATABASE IF EXISTS datawarehouse;
+CREATE DATABASE datawarehouse;
+/* crate schemas (bronze-silver-gold)*/
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
