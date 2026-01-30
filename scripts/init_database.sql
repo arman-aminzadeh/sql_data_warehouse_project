b719870af510d67d28e86fb99e41ec55a1ebca4a
Arman Aminzadeh <60994642+arman-aminzadeh@users.noreply.github.com>
2026-01-30 04:09:35 +0100

    Delete scripts/init_database.sql

--- scripts/init_database.sql
+++
@@ -1,9 +0,0 @@
-/*Create Database*/
-
-DROP DATABASE IF EXISTS datawarehouse;
-CREATE DATABASE datawarehouse;
-/* crate schemas (bronze-silver-gold)*/
-
-CREATE SCHEMA bronze;
-CREATE SCHEMA silver;
-CREATE SCHEMA gold;
